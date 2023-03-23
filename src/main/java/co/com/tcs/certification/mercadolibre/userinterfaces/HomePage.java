bc6f6320f3a30cd76f95a0a6d06264d865dfadc4
2616479 <2616479@tcs.com>
2023-03-23 15:57:18 -0500

    Map Target of subcategory dinamically

---
+++ src/main/java/co/com/tcs/certification/mercadolibre/userinterfaces/HomePage.java
@@ -0,0 +1,10 @@
+package co.com.tcs.certification.mercadolibre.userinterfaces;
+
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class HomePage {
+
+    public static final Target CATEGORIES_OPTIONS = Target.the("Menu of the categories").located(By.className("nav-menu-categories-link"));
+    public static final Target CATEGORY_OPTION = Target.the("Select category option").located(By.xpath("//a[text()='{0}']"));
+}
