f9f7d94bbcd1bbfa9bb51215e56f95d32fe20b62
2616479 <2616479@tcs.com>
2023-03-24 16:30:52 -0500

    Some fixes

--- src/main/java/co/com/tcs/certification/mercadolibre/userinterfaces/HomePage.java
+++ src/main/java/co/com/tcs/certification/mercadolibre/userinterfaces/HomePage.java
@@ -8,4 +8,5 @@ public class HomePage {
     public static final Target CATEGORIES_OPTIONS = Target.the("Menu of the categories").located(By.className("nav-menu-categories-link"));
     public static final Target CATEGORY_OPTION = Target.the("Select category option").locatedBy("//a[text()='{0}']");
     public static final Target SUBCATEGORIES = Target.the("Subcategories").locatedBy("//h3[@class='title']");
+    //public static final Target LBL_INMUEBLLES = Target.the("Scroll to Inmuebles").located(By.xpath("//a[contains(text(), 'Inmuebles')]"));
 }
